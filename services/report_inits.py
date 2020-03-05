483659b50d22077d079513c7518aea2b211d229c
Gabriel Lins <gabriel.lins97@gmail.com>
2020-03-05 00:51:03 -0300

    report inits now use generic wait conditions

--- services/report_inits.py
+++ services/report_inits.py
@@ -8,6 +8,9 @@ from tps.models import Report
 from tps.auxilliary import *
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.keys import Keys
 import time
@@ -21,8 +24,8 @@ driver = webdriver.Firefox(firefox_options=options)
 print('@driver load')
 driver.get('https://soundcloud.com/signin')
 print('@soundcloud')
-time.sleep(2)
 
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'sc-button-google')))
 gmail_login = driver.find_element_by_class_name('sc-button-google')
 gmail_login.click()
 print('@soundcloud -> gmail login requested')
@@ -31,17 +34,17 @@ time.sleep(5)
 before, after = driver.window_handles[0], driver.window_handles[1]
 driver.switch_to_window(after)
 
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'identifierId')))
 login = driver.find_element_by_id('identifierId')
 login.send_keys('romullo.quimica')
 login.send_keys(Keys.RETURN)
 print('@soundcloud -> gmail username ok')
-time.sleep(5)
 
+WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, 'password')))
 password = driver.find_element_by_name('password')
 password.send_keys('qazxsaq5601')
 password.send_keys(Keys.RETURN)
 print('@soundcloud -> gmail pw ok')
-time.sleep(2)
 driver.switch_to_window(before)
 # -------------------
 
@@ -58,13 +61,11 @@ while True:
         for file_ in files['items']:
             if 'TPS' in file_['title'] and 'https://docs.google.com/forms' in file_['alternateLink']:
                 result.append(file_)
-                print('adding...', file_['title'])
 
         page_token = files.get('nextPageToken')
         if not page_token:
             break
-    except e:
-        print('An error occurred:', e)
+    except:
         break
 # -------------------
 
@@ -80,19 +81,19 @@ for item in result:
 
         url = 'https://docs.google.com/forms/d/{}/edit'.format(item['id'])
         driver.get(url)
-        time.sleep(1)
-
+        
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'freebirdFormeditorViewItemTitleRow')))
         title = driver.find_elements_by_class_name('freebirdFormeditorViewItemTitleRow')[1]
         title.click()
 
-        time.sleep(1)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'freebirdFormeditorViewQuestionFooterWide')))
         check_answers_btn = driver.find_elements_by_class_name('freebirdFormeditorViewQuestionFooterWide')
         for item in check_answers_btn:
             if item.text == 'Chave de resposta':
                 item.click()
-                time.sleep(1)
                 break
 
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'freebirdFormeditorViewAssessmentGridbodyCorrectAnswerToggle')))
         answers = driver.find_elements_by_class_name('freebirdFormeditorViewAssessmentGridbodyCorrectAnswerToggle')
         correct_answers = ['ABCDE'[[answer.get_attribute('aria-checked') for answer in answers[i:i+5]].index('true')] for i in range(0, 50, 5)]
         form_correct_answers = {'correct_answer_' + str(i + 1): correct_answers[i] for i in range(0, 10)}
@@ -123,7 +124,9 @@ for item in result:
         report.save()
         print('DONE')
 
-    except:
-        print('FAIL')
+    except Exception as e:
+        print('FAIL', e)
         pass
 # -------------------
+
+driver.quit()
